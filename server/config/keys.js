23ee646153bcf714eeaeaf4b879db1f8f67bf08f
Pratik Bhagwat <pratikbhagwat4@gmail.com>
2021-02-20 18:19:18 -0500

    misc

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI:
-      "mongodb+srv://appmycourses:ritappmycourses@cluster0.73eob.mongodb.net/mycourses?retryWrites=true&w=majority",
-    secretOrKey: "bdhbkjsbkfbksdfjdfsfakljbgfsfajfbhvdnksedn",
-    ipAddress:"192.168.1.69"
-  };
